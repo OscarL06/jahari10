08585a94c2c85c00566e7026b976e3684b98d4b7
Oscar <oscarl06@gmail.com>
2023-03-04 19:42:11 -0600

    updated profile links in navigation

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    @yield('headers')
     <title>@yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @paddleJS
@@ -49,7 +50,7 @@
                     </svg>
                     Home
                  </a>
-                <a href="" @class(['flex w-full py-1 pl-3 rounded-md hover:border-l-4 hover:border-white hover:bg-purple-800 text-2xl', 'bg-purple-800' => request()->segment(1) == 'profile'])>
+                <a href="{{ route('user-profile', ['username' => Auth::user()->username]) }}" @class(['flex w-full py-1 pl-3 rounded-md hover:border-l-4 hover:border-white hover:bg-purple-800 text-2xl', 'bg-purple-800' => request()->segment(1) == 'profile'])>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
@@ -127,7 +128,7 @@
                 </svg>
                 Home
             </a>
-            <a href="" @class(['flex w-full py-1 pl-3 rounded-md hover:border-l-4 hover:border-white hover:bg-purple-800 text-xl', 'bg-purple-800' => request()->segment(1) == 'profile'])>
+            <a href="{{ route('user-profile', ['username' => Auth::user()->username]) }}" @class(['flex w-full py-1 pl-3 rounded-md hover:border-l-4 hover:border-white hover:bg-purple-800 text-xl', 'bg-purple-800' => request()->segment(1) == 'profile'])>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
